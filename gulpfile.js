db226299e038e82ce5ef96e1df91439469c85af1
Syntasu <abouma18@hotmail.com>
2018-04-17 14:19:07 +0200

    Intiial commit

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+﻿/// <binding BeforeBuild='compile-less, minify-css' />
+var gulp = require("gulp"),
+    less = require("gulp-less"),
+    min = require("gulp-cleancss");
+
+gulp.task("compile-less", function ()
+{
+    return gulp.src('Less/*.less')
+        .pipe(less())
+        .pipe(gulp.dest("Less/compiled"))
+});
+
+gulp.task('minify-css', function ()
+{
+    return gulp.src('Less/compiled/*.css')
+        .pipe(min({ compatibility: 'ie8' }))
+        .pipe(gulp.dest('Stylesheets'));
+});
